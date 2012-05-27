ecf3a410dbad165155ef0ab434afb9ed087fbc79
Alex Collins <alex.e.c@gmail.com>
2012-05-27 10:17:54 +0100

    First commi

---
+++ src/main/java/com/alexecollins/performancequiz/com/alexecollins/performancequiz/Bootstrap.java
@@ -0,0 +1,14 @@
+package com.alexecollins.performancequiz.com.alexecollins.performancequiz;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author: alexec (alex.e.c@gmail.com)
+ */
+public class Bootstrap {
+    public static final Logger LOGGER = Logger.getLogger(Bootstrap.class);
+    public static void main(String[] args) {
+
+        LOGGER.info("starting up...");
+    }
+}
